e36d9261b3fa987b6a8a26d49848d08880696dc3
alwaysmihirrkadam <mihirkadam1232@gmail.com>
2026-06-15 14:38:17 +0530

    Fix ChromaDB ENV again

--- server/services/chroma.js
+++ server/services/chroma.js
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const client = new ChromaClient({
-  host: process.env.CHROMA_URL,
-  ssl: true,
+  path: process.env.CHROMA_URL,
 });
 
 export default client;
